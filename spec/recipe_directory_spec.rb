90d090601d0db640c57b23992733831c411b556b
Barney <B.Wilton1993@gmail.com>
2023-04-25 14:10:59 +0100

    Wrote a failing test for all method in recipe_directory

---
+++ spec/recipe_directory_spec.rb
@@ -0,0 +1,30 @@
+require 'recipe_directory'
+
+def reset_recipes_table
+  seed_sql = File.read('spec/seeds.sql')
+  connection = PG.connect({host: '127.0.0.1', dbname: 'recipes_directory_test'})
+  connection.exec(seed_sql)
+end
+
+RSpec.describe RecipeDirectory do 
+  before(:each) do
+    reset_recipes_table
+  end
+
+  it "returns all the recipes" do
+    repo = RecipeDirectory.new
+
+    recipes = repo.all
+    expect(recipes.length).to eq 5
+
+    expect(recipes[0].id).to eq 1
+    expect(recipes[0].name).to eq 'Pepperoni Pizza'
+    expect(recipes[0].cooking_time).to eq 10
+    expect(recipes[0].rating).to eq 5
+    
+    expect(recipes[-1].id).to eq 5
+    expect(recipes[-1].name).to eq 'Microwave Meal'
+    expect(recipes[-1].cooking_time).to eq 4
+    expect(recipes[-1].rating).to eq 2
+  end
+end
