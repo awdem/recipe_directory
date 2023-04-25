5d258097d7930dcd9cb64aa1096075dd31608764
Barney <B.Wilton1993@gmail.com>
2023-04-25 14:23:56 +0100

    wrote all method for RecipeDirectory class

--- lib/recipe_directory.rb
+++ lib/recipe_directory.rb
@@ -1,6 +1,24 @@
+require 'recipe'
+
 class RecipeDirectory
   def all
+    recipes = []
+    
+    sql = 'SELECT * FROM recipes;'
+    result_set = DatabaseConnection.exec_params(sql, [])
 
+    result_set.each do |result|
+      recipe = Recipe.new
+
+      recipe.id = result['id'].to_i
+      recipe.name = result['name']
+      recipe.cooking_time = result['cooking_time'].to_i
+      recipe.rating = result['rating'].to_i
+
+      recipes << recipe
+    end
+
+    return recipes
   end
 
   def find_by_id(id)
